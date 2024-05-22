70afab4685c3bf8282e30476910462aae044b746
Romano <romano1@live.nl>
2024-05-22 23:52:31 +0200

    WIP samplescene

---
+++ Assets/Scripts/Models/Note.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note
+{
+	public string Name { get; private set; }
+	public string Description { get; private set; }
+}
